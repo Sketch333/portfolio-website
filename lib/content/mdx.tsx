b4d0208581ae31aaa184fc00a5f3ba5adb065af1
Syed Aon Muhammad Kazmi <syedaonm@gmail.com>
2026-09-09 03:30:02 -0700

    feat: add server-rendered MDX boundary

---
+++ lib/content/mdx.tsx
@@ -0,0 +1,7 @@
+import { compileMDX } from 'next-mdx-remote/rsc';
+import { mdxComponents } from '@/components/blog/mdx-components';
+
+export async function renderMdx(source: string) {
+  const { content } = await compileMDX({ source, components: mdxComponents });
+  return content;
+}
